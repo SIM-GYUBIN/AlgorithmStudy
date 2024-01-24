e1b0c5f7905e28c0781f9f1af55b5fa90e98c14c
Sim-Gyubin <89975936+SIM-GYUBIN@users.noreply.github.com>
2024-01-24 21:02:57 +0900

    [Bronze V] Title: Hello World, Time: 120 ms, Memory: 13952 KB -BaekjoonHub

---
+++ 백준/Bronze/2557. Hello World/Hello World.java
@@ -0,0 +1,10 @@
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        System.out.print("Hello World!");
+    }
+}
