ef4162ddec10670da07c1364a0ee108fe32b32a9
Sim-Gyubin <89975936+SIM-GYUBIN@users.noreply.github.com>
2024-01-24 17:46:11 +0900

    [Bronze V] Title: We love kriii, Time: 116 ms, Memory: 13936 KB -BaekjoonHub

---
+++ 백준/Bronze/10718. We love kriii/We love kriii.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        yeah();
+        yeah();
+    }
+
+    private static void yeah() {
+        System.out.println("강한친구 대한육군");
+    }
+}
