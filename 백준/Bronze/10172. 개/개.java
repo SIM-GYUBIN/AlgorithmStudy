523c555127f70c0dfb97fdfcfd9f4b51e018c676
Sim-Gyubin <89975936+SIM-GYUBIN@users.noreply.github.com>
2024-01-24 21:34:03 +0900

    [Bronze V] Title: 개, Time: 112 ms, Memory: 13892 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.java
@@ -0,0 +1,15 @@
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        StringTokenizer st = new StringTokenizer(br.readLine());
+
+
+        System.out.println("|\\_/|\n" +
+                "|q p|   /}\n" +
+                "( 0 )\"\"\"\\\n" +
+                "|\"^\"`    |\n" +
+                "||_/=\\\\__|");
+    }
+}
