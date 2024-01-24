a8fe492fe0c7c7bba8c2781a2435b1b898a5055e
Sim-Gyubin <89975936+SIM-GYUBIN@users.noreply.github.com>
2024-01-24 18:01:03 +0900

    [Bronze V] Title: A-B, Time: 124 ms, Memory: 14292 KB -BaekjoonHub

---
+++ 백준/Bronze/1001. A－B/A－B.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int 에이 = Integer.parseInt(st.nextToken());
+        int 비 = Integer.parseInt(st.nextToken());
+
+
+        System.out.println(에이 - 비);
+    }
+
+}
