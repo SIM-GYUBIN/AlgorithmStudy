607b6e5e6a7ad17491eeee9f38f5d769cfa5ef56
Sim-Gyubin <89975936+SIM-GYUBIN@users.noreply.github.com>
2024-05-04 19:16:07 +0900

    [Gold IV] Title: N-Queen, Time: 5552 ms, Memory: 14800 KB -BaekjoonHub

---
+++ 백준/Gold/9663. N－Queen/N－Queen.java
@@ -0,0 +1,46 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+   static int[] board;
+   static int N;
+   static int result;
+
+   public static void main(String[] args) throws Exception {
+      StringBuilder sb = new StringBuilder();
+      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+      N = Integer.parseInt(br.readLine());
+      board = new int[N];
+      dfs(0);
+      System.out.println(result);
+   }
+
+   private static void dfs(int x) {
+      if (x == N) {
+         result++;
+         return;
+      }
+
+      loop_row:
+      for (int y = 0; y < N; y++) {
+         board[x] = y;   //2차원배열같은 1차원 배열 (이전의 퀸과 놓고 싶은 자리의 퀸의 비교를 용이하게 하기 위해)
+         for (int beforeX = 0; beforeX < x; beforeX++) { //이전 행들의 퀸 위치와 비교
+            int beforeY = board[beforeX];
+            if (beforeY==y) {   //같은 행
+               board[x] = 0;
+               if(y==N-1)
+                  return;
+               continue loop_row;
+            }
+            if (Math.abs(x-beforeX) == Math.abs(y - beforeY)) { //열의 차와 행의 차가 같은 경우 대각선 (절대값 필요)
+               board[x] = 0;
+               if(y==N-1)
+                  return;
+               continue loop_row;
+            }
+         }
+         //통과된 경우
+         dfs(x+1);
+      }
+   }
+}
